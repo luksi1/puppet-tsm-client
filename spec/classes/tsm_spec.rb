d24989c8e852f1dcb1f5afedbb211b3847aeaeab
Luke Simmons <luke.simmons@vgregion.se>
2016-10-07 16:08:11 +0200

    added package tests.

--- spec/classes/tsm_spec.rb
+++ spec/classes/tsm_spec.rb
@@ -1,11 +1,30 @@
 require 'spec_helper'
 
 describe 'tsm', :type => :class  do
-  let(:facts) {{:hostname => 'my_client'}}
+  let :facts do
+    {
+      :hostname => 'client.domain.com',
+      :osfamily => 'Debian',
+      :operatingsystem => 'Ubuntu',
+      :architecture => 'x86_64'
+    }
+  end
   let(:server_name) {'foo'}
   let(:tcp_server_address) {'tsm_server.domain.com'}
   context 'should compile' do
     it { should compile }
+  end
+  context 'should contain gskcrypt64' do
+    it {is_expected.to contain_package("gskcrypt64")}
+  end
+  context 'should contain gskssl64' do
+    it {is_expected.to contain_package("gskssl64")}
+  end
+  context 'should contain tivsm-ba' do
+    it {is_expected.to contain_package("tivsm-ba")}
+  end
+  context 'should contain tivsm-api64' do
+    it {is_expected.to contain_package("tivsm-api64")}
   end
 end
 
