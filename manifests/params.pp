1462e3a9e405c1944365148049da6b6e73b996f8
Luke Simmons <luke.simmons@vgregion.se>
2016-10-07 15:45:59 +0200

    moved most options to init.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,32 +1,4 @@
 class tsm::params {
-  $server_name = undef
-  $tcp_server_address = undef
-  $node_name = undef
-  $managed_services = 'WEBCLIENT SCHEDULE'
-  $memory_efficient_backup = 'yes'
-  $comm_method = 'TCPip'
-  $tcp_port = '1500'
-  $password_access = 'generate'
-  $sched_mode = 'POLLING'
-  $domain = ['ALL-LOCAL']
-  $tcp_client_port = '1501'
-  $http_port = '1581'
-  $tcp_window_size = '63'
-  $tcp_no_delay = 'no'
-  $resource_util = '5'
-  $txn_byte_limit = '25600'
-  $log_dir = '/var/log'
-  $error_log_name = '/var/log/tsm/err.log'
-  $sched_log_name = '/var/log/tsm/sched.log'
-  $sched_log_ret = '30'
-  $error_log_ret = '30'
-  $incl_excl = '/opt/tivoli/tsm/client/ba/bin/inexclude_file'
-  $service_ensure = 'running'
-  $service_enable = true
-  $service_name = 'dsmcad'
-  $service_provider = undef
-  $service_manage = true
-
   # Packages
   $gskcrypt_package = $architecture ? {
     'i386'   => 'gskcrypt',
