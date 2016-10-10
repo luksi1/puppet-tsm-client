e8e937358c9c903463d2aff183737f626ea05758
luksi1 <luke.simmons@gmail.com>
2016-10-10 21:32:48 +0200

    moved to file_line. augeas spacevars lense does not accept three spaces

--- manifests/config.pp
+++ manifests/config.pp
@@ -30,34 +30,39 @@ class tsm::config inherits tsm {
     owner  => root,
     group  => root,
     mode   => '0644',
-  } ->
-  augeas { "dsm.sys":
-    lens    => "Spacevars.lns",
-    incl    => "/opt/tivoli/client/ba/bin/dsm.sys",
-    changes => [
-      "set SErvername $tsm::server_name",
-      "set MANAGEDSERVICES $tsm::managed_services",
-      "set MEMORYEFFICIENTBACKUP $tsm::memory_efficient_backup",
-      "set COMMMethod $tsm::comm_method",
-      "set TCPPort $tsm::tcp_port",
-      "set TCPServeraddress $tsm::tcp_server_address",
-      "set PASSWORDACCESS $tsm::password_access",
-      "set SCHEDMODE $tsm::sched_mode",
-      "set NODENAME $tsm::node_name",
-      "set DOMAIN $tsm::domain",
-      "set TCPCLIENTPORT $tsm::tcp_client_port",
-      "set HTTPPORT $tsm::http_port",
-      "set TCPWINDOWSIZE $tsm::tcp_window_size",
-      "set TCPNODELAY $tsm::tcp_no_delay",
-      "set RESOURCEUTIL $tsm::resource_util",
-      "set TXNBYTELIMIT $tsm::txn_byte_limit",
-      "set ERRORLOGNAME $tsm::error_log_name",
-      "set SCHEDLOGNAME $tsm::sched_log_name",
-      "set SCHEDLOGRET $tsm::sched_log_ret",
-      "set ERRORLOGRET $tsm::error_log_ret",
-      "set INCLExcl $tsm::incl_excl"],
-    notify  => Service[$tsm::service_name],
+  } 
+ 
+  $dsm_default_options = {
+	'path' => '/opt/tivoli/tsm/client/ba/bin/dsm.sys',
+	'ensure' => 'present',
+	'multiple' => 'false',
   }
+
+  $lines = {
+	'SErvername' => { line => "SErvername $tsm::server_name" },
+	'MANAGEDSERVICES' => { line => "MANAGEDSERVICES $tsm::managed_services" },
+        'MEMORYEFFICIENTBACKUP' => { line => "MEMORYEFFICIENTBACKUP $tsm::memory_efficient_backup" },
+	'COMMMethod' => {line => "COMMMethod $tsm::comm_method" },
+        'TCPPort' => {line => "TCPPort $tsm::tcp_port" },
+	'TCPServeraddress' => {line => "TCPServeraddress $tsm::tcp_server_address"},
+	'PASSWORDACCESS' => {line => "PASSWORDACCESS $tsm::password_access"},
+        'SCHEDMODE' => { line => "SCHEDMODE $tsm::sched_mode" },
+        'NODENAME' => { line => "NODENAME $tsm::node_name" },
+        'DOMAIN' => {line => "DOMAIN $tsm::domain" },
+        'TCPCLIENTPORT' => { line => "TCPCLIENTPORT $tsm::tcp_client_port" },
+        'HTTPPORT' => { line => "HTTPPORT $tsm::http_port" },
+        'TCPWINDOWSIZE' => {line => "TCPWINDOWSIZE $tsm::tcp_window_size" },
+        'TCPNODELAY' => {line => "TCPNODELAY $tsm::tcp_no_delay"},
+        'RESOURCEUTIL' => {line => "RESOURCEUTIL $tsm::resource_util"},
+        'TXNBYTELIMIT' => {line => "TXNBYTELIMIT $tsm::txn_byte_limit" },
+        'ERRORLOGNAME' => {line => "ERRORLOGNAME $tsm::error_log_name"},
+        'SCHEDLOGNAME' => {line => "SCHEDLOGNAME $tsm::sched_log_name"},
+        'SCHEDLOGRET' => {line => "SCHEDLOGRET $tsm::sched_log_ret"},
+        'ERRORLOGRET' => {line => "ERRORLOGRET $tsm::error_log_ret"},
+        'INCLExcl' => {line => "INCLExcl $tsm::incl_excl"}
+  }
+
+  create_resources(file_line, $lines, $dsm_default_options)
 
   file { '/opt/tivoli/tsm/client/ba/bin/dsm.opt':
     owner => root,
