1590afeaadafa5c4a52bbe7f695ccb3ed21158c4
Luke Simmons <luke.simmons@vgregion.se>
2017-09-04 12:56:00 +0200

    add template test

--- spec/classes/tsm_spec.rb
+++ spec/classes/tsm_spec.rb
@@ -12,18 +12,21 @@ describe 'tsm', :type => :class  do
     let(:facts) {{:osfamily => 'RedHat', :architecture => 'x86_64' }}
     it {is_expected.to contain_package("TIVsm-BA")}
     it {is_expected.to contain_package("TIVsm-API64")}
-    it {should contain_file('/opt/tivoli/tsm/client/ba/bin/dsm.sys').with_content("NODENAME foo")}
   end
 
   context 'Suse x86_64 contain the correct packages' do
     let(:facts) {{:osfamily => 'Suse', :architecture => 'x86_64' }}
     it {is_expected.to contain_package("TIVsm-BA")}
     it {is_expected.to contain_package("TIVsm-API64")}
+  end
+
+  context 'dsm.sys should be configured correctly' do
+    let(:facts) {{:osfamily => 'Debian', :architecture => 'x86_64', :hostname => 'foo' }}
     it {should contain_file('/opt/tivoli/tsm/client/ba/bin/dsm.sys').with_content("NODENAME foo")}
   end
 
   context 'logs files should be created' do
-    let(:facts) {{:osfamily => 'Debian', :architecture => 'x86_64', :hostname => 'foo' }}
+    let(:facts) {{:osfamily => 'Debian', :architecture => 'x86_64' }}
     it { is_expected.to contain_file('/opt/tivoli/tsm/client/ba/bin/inexclude_file') }
     it { is_expected.to contain_file('/opt/tivoli/tsm/client/ba/bin/dsm.opt') }
   end
