dd7b96041546a6214c08533f2dfb57e40dd00483
Luke Simmons <luke.simmons@vgregion.se>
2019-01-11 13:03:14 +0100

    tivsm-api needs to be before tivsm-ba installation for Redhat based distros

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,8 +3,8 @@ class tsm::install inherits tsm {
   if $::tsm::package_manage {
     package { $::tsm::gskcrypt_package: ensure => $::tsm::gskcrypt_package_ensure, }
     package { $::tsm::gskssl_package: ensure => $::tsm::gskssl_package_ensure, }
-    package { $::tsm::tivsm_ba_package: ensure => $::tsm::tivsm_ba_package_ensure, }
     package { $::tsm::tivsm_api_package: ensure => $::tsm::tivsm_api_package_ensure, }
+    package { $::tsm::tivsm_ba_package: ensure => $::tsm::tivsm_ba_package_ensure, }
   }
 
 }
