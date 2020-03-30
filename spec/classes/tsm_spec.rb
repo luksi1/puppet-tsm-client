afa5ff3feabb676307c47885f97037a40868bd5d
luksi1 <luke.simmons@vgregion.se>
2020-03-30 15:37:58 +0200

    Added operatingsystem facter variable for Puppet 6

--- spec/classes/tsm_spec.rb
+++ spec/classes/tsm_spec.rb
@@ -7,23 +7,28 @@ describe 'tsm' do
     }
   end
 
-  context 'Ubuntu x86_64 should contain the correct packages' do
+  context 'Ubuntu x86_64' do
     let(:facts) do
       {
         osfamily: 'Debian',
         architecture: 'x86_64',
+        operatingsystem: 'Ubuntu'
       }
     end
 
     it { is_expected.to contain_package('tivsm-ba') }
     it { is_expected.to contain_package('tivsm-api64') }
+    it { is_expected.to contain_file('/opt/tivoli/tsm/client/ba/bin/inexclude_file') }
+    it { is_expected.to contain_file('/opt/tivoli/tsm/client/ba/bin/dsm.opt') }
+    it { is_expected.to contain_service('dsmcad').with_ensure('running') }
   end
 
-  context 'RedHat x86_64 contain the correct packages' do
+  context 'RedHat/CentOS x86_64' do
     let(:facts) do
       {
         osfamily: 'RedHat',
         architecture: 'x86_64',
+        operatingsystem: 'CentOS'
       }
     end
 
@@ -31,38 +36,16 @@ describe 'tsm' do
     it { is_expected.to contain_package('TIVsm-API64') }
   end
 
-  context 'Suse x86_64 contain the correct packages' do
+  context 'Suse x86_64' do
     let(:facts) do
       {
         osfamily: 'Suse',
         architecture: 'x86_64',
+        operatingsystem: 'SLES'
       }
     end
 
     it { is_expected.to contain_package('TIVsm-BA') }
     it { is_expected.to contain_package('TIVsm-API64') }
-  end
-
-  context 'logs files should be created' do
-    let(:facts) do
-      {
-        osfamily: 'Debian',
-        architecture: 'x86_64',
-      }
-    end
-
-    it { is_expected.to contain_file('/opt/tivoli/tsm/client/ba/bin/inexclude_file') }
-    it { is_expected.to contain_file('/opt/tivoli/tsm/client/ba/bin/dsm.opt') }
-  end
-
-  context 'services should be started' do
-    let(:facts) do
-      {
-        osfamily: 'Debian',
-        architecture: 'x86_64',
-      }
-    end
-
-    it { is_expected.to contain_service('dsmcad').with_ensure('running') }
   end
 end
